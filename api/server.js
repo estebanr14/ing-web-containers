1eac36a2e8887b785f0633070e072492d388922e
Your Name <you@example.com>
2019-09-19 13:56:33 -0500

    segunda clase

--- api/server.js
+++ api/server.js
@@ -4,7 +4,7 @@ const express = require('express'),
     cors = require('cors'),
     mongoose = require('mongoose'),
     config = require('./DB');
-const businessRoute = require('./routes/business.route');
+const businessRoute = require('./routes/business.routes');
 mongoose.Promise = global.Promise;
 mongoose.connect(config.DB, { useNewUrlParser: true }).then(
     () => { console.log('Database is connected') },
